15916706aeba8fd9dbcf680a8c0a1d18b3b5bca8
MatheusVilas <matheus@vilasboas.dev>
2021-03-21 11:20:36 -0300

    first commit

--- App.js
+++ App.js
@@ -1,47 +1,159 @@
 import Animated, {
   useSharedValue,
-  withTiming,
   useAnimatedStyle,
-  Easing,
+  useAnimatedScrollHandler,
+  useDerivedValue,
+  useAnimatedRef,
+  scrollTo,
 } from "react-native-reanimated";
-import { View, Button } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+
+import { categories } from "./config";
 
 export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
+  const tabScrollViewRef = useAnimatedRef();
+  const screenScrollViewRef = useAnimatedRef();
+  const activeCategoryIndex = useSharedValue(0);
+  const headerHeight = useSharedValue(400);
+  const y = useSharedValue(0);
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: ({ contentOffset: { y: value } }) => {
+      y.value = value;
+    },
+  });
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
-
-  const style = useAnimatedStyle(() => {
+  const fixedStyle = useAnimatedStyle(() => {
+    if (y.value > headerHeight.value) {
+      return {
+        top: y.value - headerHeight.value,
+      };
+    }
     return {
-      width: withTiming(randomWidth.value, config),
+      top: 0,
     };
   });
 
+  function setCurrentCategoryActive(index) {
+    "worklet";
+    activeCategoryIndex.value = index;
+  }
+
+  useDerivedValue(() => {
+    const scrollPosition = activeCategoryIndex.value * 130;
+
+    scrollTo(tabScrollViewRef, scrollPosition, 0, true);
+  }, [activeCategoryIndex]);
+
+  return (
+    <SafeAreaView>
+      <Animated.ScrollView
+        ref={screenScrollViewRef}
+        onScroll={onScroll}
+        scrollEventThrottle={16}
+      >
+        <View style={{ position: "relative" }}>
+          <View style={{ backgroundColor: "yellow", height: 400 }}></View>
+          <Animated.View
+            style={[
+              fixedStyle,
+              {
+                height: 70,
+                backgroundColor: "cyan",
+                width: "100%",
+                zIndex: 1000,
+              },
+            ]}
+          >
+            <Animated.ScrollView
+              ref={tabScrollViewRef}
+              showsHorizontalScrollIndicator={false}
+              horizontal
+              style={{}}
+            >
+              {categories.map((category) => (
+                <View
+                  style={{
+                    width: 130,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    borderColor: "white",
+                    borderWidth: 2,
+                  }}
+                  key={category.id}
+                >
+                  <Text>{category.name}</Text>
+                </View>
+              ))}
+            </Animated.ScrollView>
+          </Animated.View>
+
+          {categories.map((category, index) => (
+            <CategoryItem
+              key={category.id}
+              {...{
+                category,
+                y,
+                index,
+                activeCategoryIndex,
+                setCurrentCategoryActive,
+                screenScrollViewRef,
+              }}
+            />
+          ))}
+        </View>
+      </Animated.ScrollView>
+    </SafeAreaView>
+  );
+}
+
+function CategoryItem({
+  id,
+  y,
+  category,
+  setCurrentCategoryActive,
+  activeCategoryIndex,
+  index,
+  screenScrollViewRef,
+}) {
+  const [anchorPosition, setAnchorPosition] = useState(0);
+  const [listHeight, setListHeight] = useState(0);
+
+  useDerivedValue(() => {
+    if (y.value > anchorPosition && y.value <= anchorPosition + listHeight) {
+      if (activeCategoryIndex.value !== index) setCurrentCategoryActive(index);
+    }
+  }, [y]);
+
+  function handleScroll() {
+    scrollTo(screenScrollViewRef, 0, screenScrollViewRef, true);
+  }
+
   return (
     <View
+      onLayout={({
+        nativeEvent: {
+          layout: { y: anchor, height },
+        },
+      }) => {
+        setAnchorPosition(anchor);
+        setListHeight(height);
+      }}
+      key={id}
       style={{
-        flex: 1,
+        backgroundColor: "brown",
+        height: 1000,
+        borderBottomColor: "red",
+        borderBottomWidth: 2,
         alignItems: "center",
         justifyContent: "center",
-        flexDirection: "column",
       }}
     >
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
+      <TouchableOpacity onPress={handleScroll}>
+        <Animated.Text style={{ fontSize: 40, color: "white" }}>
+          {anchorPosition} ~ {listHeight}
+        </Animated.Text>
+      </TouchableOpacity>
     </View>
   );
 }
---
+++ config.js
@@ -0,0 +1,138 @@
+export const categories = [
+  {
+    id: "01",
+    name: "Bebidas",
+    products: [
+      {
+        id: "01_01",
+        name: "Coca-Cola",
+      },
+      {
+        id: "01_02",
+        name: "Coca-Cola",
+      },
+      {
+        id: "01_03",
+        name: "Coca-Cola",
+      },
+      {
+        id: "01_04",
+        name: "Coca-Cola",
+      },
+      {
+        id: "01_05",
+        name: "Coca-Cola",
+      },
+      {
+        id: "01_06",
+        name: "Coca-Cola",
+      },
+      {
+        id: "01_07",
+        name: "Coca-Cola",
+      },
+    ],
+  },
+  {
+    id: "02",
+    name: "Salgados",
+    products: [
+      {
+        id: "02_01",
+        name: "Coca-Cola",
+      },
+      {
+        id: "02_02",
+        name: "Coca-Cola",
+      },
+      {
+        id: "02_03",
+        name: "Coca-Cola",
+      },
+      {
+        id: "02_04",
+        name: "Coca-Cola",
+      },
+      {
+        id: "02_05",
+        name: "Coca-Cola",
+      },
+      {
+        id: "02_06",
+        name: "Coca-Cola",
+      },
+      {
+        id: "02_07",
+        name: "Coca-Cola",
+      },
+    ],
+  },
+  {
+    id: "03",
+    name: "Doces e Docinhos",
+    products: [
+      {
+        id: "03_01",
+        name: "Coca-Cola",
+      },
+      {
+        id: "03_02",
+        name: "Coca-Cola",
+      },
+      {
+        id: "03_03",
+        name: "Coca-Cola",
+      },
+      {
+        id: "03_04",
+        name: "Coca-Cola",
+      },
+      {
+        id: "03_05",
+        name: "Coca-Cola",
+      },
+      {
+        id: "03_06",
+        name: "Coca-Cola",
+      },
+      {
+        id: "03_07",
+        name: "Coca-Cola",
+      },
+    ],
+  },
+  {
+    id: "04",
+    name: "Cachorro Quente",
+    products: [
+      {
+        id: "04_01",
+        name: "Coca-Cola",
+      },
+      {
+        id: "04_02",
+        name: "Coca-Cola",
+      },
+      {
+        id: "04_03",
+        name: "Coca-Cola",
+      },
+      {
+        id: "04_04",
+        name: "Coca-Cola",
+      },
+      {
+        id: "04_05",
+        name: "Coca-Cola",
+      },
+      {
+        id: "04_06",
+        name: "Coca-Cola",
+      },
+      {
+        id: "04_07",
+        name: "Coca-Cola",
+      },
+    ],
+  },
+];
